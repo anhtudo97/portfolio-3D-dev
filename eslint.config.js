d93edd902dbd3f9064e43837a6de4a2948b472fa
Alex Dou <anhtudo97@gmail.com>
2025-08-06 00:41:00 +0700

    feat: setup config

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', '.env', '.env.local', '.env.development.local', '.env.test.local', '.env.production.local']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
